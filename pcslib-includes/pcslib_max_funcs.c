38bc36a83ebfab7da3b6a2888bcf3f411294e414
Antonio Ortega Brook <antonioortegabrook@antonios-macbook-pro.local>
2017-03-17 19:53:53 -0300

    Created the type t_ptr_mess for wrapping pointers in an atom-like struct. Also defined methods for accessing data in them.

---
+++ pcslib-includes/pcslib_max_funcs.c
@@ -0,0 +1,70 @@
+/*  pcslib_max_funcs.h by Antonio Ortega Brook - March 2017
+ 
+ --- Only used for Max objects ---
+ 
+ These are the methods for set and retrieve pointer data from t_ptr_mess,
+ an atom-like struct to pass pointers from one object to another in a *safe* way
+ (see pcslib_max_types.h)
+ */
+
+PCS *ptr_mess_getpcs(const t_ptr_mess *p);
+CM *ptr_mess_getcm(const t_ptr_mess *p);
+
+int ptr_mess_setpcs(t_ptr_mess *p, PCS *s);
+int ptr_mess_setcm(t_ptr_mess *p, CM *s);
+
+
+/** retrieves a PCS pointer from a t_ptr_mess;
+    returns NULL if type doesn't match
+ */
+PCS *ptr_mess_getpcs(const t_ptr_mess *p) {
+    PCS *ptr = NULL;
+    
+    if (p->p_type == P_PCS)
+        ptr = p->p_w.pcs;
+    
+    return ptr;
+}
+
+/** retrieves a CM pointer from a t_ptr_mess;
+ returns NULL if type doesn't match
+ */
+CM *ptr_mess_getcm(const t_ptr_mess *p)
+{
+    CM *ptr = NULL;
+
+    if (p->p_type == P_CM)
+        ptr = p->p_w.cm;
+
+    return ptr;
+}
+
+/** inserts a PCS pointer into a t_ptr_mess and changes its type to P_PCS
+ */
+int ptr_mess_setpcs(t_ptr_mess *p, PCS *s)
+{
+    if (!p)
+        return -100;    // -100 = null ptr_mess
+    if (!s)
+        return -101;    // -101 = null PCS
+    
+    p->p_type = P_PCS;
+    p->p_w.pcs = s;
+    
+    return 0;           // 0 = success!
+}
+
+/** inserts a CM pointer into a t_ptr_mess and changes its type to P_CM
+ */
+int ptr_mess_setcm(t_ptr_mess *p, CM *s)
+{
+    if (!p)
+        return -100;    // -100 = null ptr_mess
+    if (!s)
+        return -102;    // -101 = null CM
+    
+    p->p_type = P_CM;
+    p->p_w.cm = s;
+    
+    return 0;           // 0 = success!
+}
---
+++ pcslib-includes/pcslib_max_types.h
@@ -0,0 +1,33 @@
+/*  pcslib_max_types.h by Antonio Ortega Brook - March 2017
+ 
+    --- Only used for Max objects ---
+ 
+    These data types are used to send pointers from one object to
+    another in a *safe* way, instead of passing them as symbols inside
+    atoms. There are also methods for set and retrieve pointer data
+    from them (see pcslib_max_funcs.c)
+ */
+
+
+/** PCSlib types
+ */
+typedef enum {
+    P_PCS = 30,     // < pointer to a PCS
+    P_CM            // < pointer to a CM
+} e_pcslib_ptr_types;
+
+/** Union for packing the PCSlib types defined above
+ */
+union pcs_word {
+    PCS *pcs;
+    CM *cm;
+};
+
+/** Atom-like struct for wrapping pointers to be sent to other objects;
+    we cast *t_ptr_mess to *t_atom to pass it through an outlet_anything and
+    use A_GIMME to receive them
+ */
+typedef struct ptr_mess {
+    short           p_type;
+    union pcs_word  p_w;
+} t_ptr_mess;
