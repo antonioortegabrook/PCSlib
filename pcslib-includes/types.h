a80b2bd10d65695448b14a2efb801fe0c2caa3fb
Antonio Ortega Brook <antonioortegabrook@antonios-macbook-pro.local>
2017-04-13 19:45:42 -0300

    Added new field to t_pcs for storing pitch content without EOPs

--- pcslib-includes/types.h
+++ pcslib-includes/types.h
@@ -9,8 +9,10 @@ typedef struct {
         int consistent;         //  Wether its data is consistent or not
         int ncar;               //  Cardinal number
         int nord;               //  Ordinal number
-        int *pitch_content;     //  PCS as delivered
-        int nelem;              //  Number of pcs and EOPs
+        int *pcs_delivered;     //  PCS as delivered
+        int nelem;              //  Number of pcs and EOPs in pcs_delivered
+        int *pitch_content;     //  Pitch content only (no EOPs)
+        int npitches;           //  Number of pcs in pitch_content
         int prime_form[12];     //  Prime form (we no longer write EOC in prime form)
         int t;                  //  T value respect of the prime form
         int inverted;           //  True if inv, false if not
