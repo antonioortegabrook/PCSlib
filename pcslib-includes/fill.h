96b756021a92ce3c266236d01e9677af71957d8e
Antonio Ortega Brook <antonioortegabrook@antonios-macbook-pro.local>
2017-04-17 02:15:42 -0300

    added remove_eops()

--- pcslib-includes/fill.h
+++ pcslib-includes/fill.h
@@ -12,3 +12,9 @@ int pcs_fill_from_name(t_pcs *pcs, int car, int ord, int tr, int inv);
         Fill a t_pcs from pitch content
  */
 int pcs_fill_from_pitch_content(t_pcs *pcs, int *vector, int nelem);
+
+/**     Remove EOPs. Return a pointer to the new array and write number of
+        pitches to an int passed as parameter
+ 
+ */
+int * remove_eops(int *delivered, int n, int *npitches);
